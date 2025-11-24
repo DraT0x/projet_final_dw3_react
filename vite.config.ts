652ff1b0da3727fbfd29a23d87b9614880499b65
DraT0x <felixdupras@gmail.com>
2025-11-24 15:52:19 -0500

    ajustements pour déploiement Azure

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-
-// server, proxy trouvé par Claude.AI
 export default defineConfig({
-  plugins: [react()],
-  }
+  plugins: [react()], 
 })
